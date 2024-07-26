18c084e575dd51c8f93fe29a8d8bc002cd0bca4a
Corey Hudson <coreymhudson@gmail.com>
2024-07-25 20:21:49 -0500

    Removed sidebar

--- src/App.js
+++ src/App.js
@@ -11,37 +11,10 @@ import Projects from './components/Projects';
 import Contact from './components/Contact';
 
 const App = () => {
-  const sidebarStyle = {
-    width: '200px',
-    position: 'fixed',
-    height: '100vh',
-    background: 'linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url("/nav-background-new.jpg") no-repeat center center',
-    backgroundSize: 'cover',
-    color: '#fff',
-    paddingTop: '20px',
-    boxShadow: '2px 0 5px rgba(0,0,0,0.1)',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    paddingBottom: '20px'
-  };
-
   return (
     <div className="container">
-      <nav className="sidebar" style={sidebarStyle}>
-        <ul>
-          <li><a href="#summary">Professional Summary</a></li>
-          <li><a href="#about">About Me</a></li>
-          <li><a href="#experience">Professional Experience</a></li>
-          <li><a href="#education">Academic Background</a></li>
-          <li><a href="#skills">Key Skills</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-      </nav>
+      <Header />
       <div className="content">
-        <Header />
         <section id="summary"><Summary /></section>
         <section id="about"><About /></section>
         <section id="experience"><Experience /></section>
