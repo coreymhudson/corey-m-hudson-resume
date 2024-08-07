6a4ea67498838c4971265bf3281757a3a8f44610
Corey Hudson <coreymhudson@gmail.com>
2024-08-07 14:39:44 -0500

    Academic

--- src/components/Education.js
+++ src/components/Education.js
@@ -5,12 +5,18 @@ const Education = () => {
         <section className="education">
             <h2>Academic Background</h2>
             <div className="school">
-                <h3>M.S. in Data Science, University of Somewhere</h3>
-                <p>2014 - 2016</p>
+                <h3>Ph.D. University of Missouri Data Science and Informatics</h3>
+                <h4>Specialization in bioinformatics and genomics</h4>
+                <p>2009 - 2013</p>
             </div>
             <div className="school">
-                <h3>B.S. in Computer Science, University of Anywhere</h3>
-                <p>2010 - 2014</p>
+                <h3>M.S. University of Missouri Anthropology</h3>
+                <h4>Specialization in archaeology and ancient DNA</h4>
+                <p>2006 - 2009</p>
+            </div>
+            <div className="school">
+                <h3>B.S. Truman State University Anthropology</h3>
+                <p>2004 - 2006</p>
             </div>
         </section>
     );
