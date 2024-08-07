874b7df5884600caf8eadec9728c7759473305b2
Corey Hudson <coreymhudson@gmail.com>
2024-08-07 13:31:53 -0500

    Experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -5,21 +5,90 @@ const Experience = () => {
         <section className="experience">
             <h2>Professional Experience</h2>
             <div className="job">
-                <h3>Senior Data Scientist, ABC Corp</h3>
-                <p>January 2019 - Present</p>
+                <h3>Principal Member of the Technical Staff, Sandia National Laboratories</h3>
+                <p>February 2023 - Present</p>
                 <ul>
-                    <li>Led a team of data scientists in developing predictive models for disease outbreaks.</li>
-                    <li>Implemented machine learning algorithms to personalize medical treatments.</li>
-                    <li>Improved patient outcomes through data-driven insights and decision-making.</li>
+                    <li>Developed biotechnology data collecting, security and safety training for a large number of international diagnostics and biotechnology labs.</li>
+                    <li>Engaged with the US Department of State and the Defense Threat Reduction Agency to deploy safe biotechnology trainings worlwide.</li>
+                    <li>Worked with partners in Brazil, South Africa, Argentina, Gabon, India, Philippines, Moracco and in regional African, Asian and South American laboratory consortia.</li>
                 </ul>
             </div>
             <div className="job">
-                <h3>Data Scientist, XYZ Inc.</h3>
-                <p>June 2016 - December 2018</p>
+                <h3>Program Manager - Defense Sciences Office, DARPA</h3>
+                <p>August 2022 - Febuary 2023</p>
                 <ul>
-                    <li>Conducted data analysis and statistical modeling to support business decisions.</li>
-                    <li>Developed data visualization tools to communicate insights to stakeholders.</li>
-                    <li>Collaborated with cross-functional teams to implement data-driven solutions.</li>
+                    <li>Oversaw the Automated Molecular Design program and SBIR XL initiatives.</li>
+                    <li>Drove strategic development, performance evaluation, budgeting, and scientific assessments across multiple projects.</li>
+                    <li>Managed a core group of Scientific, Engineering and Technical advisors.</li>
+                </ul>
+            </div>
+            <div className="job">
+                <h3>Manager - Microbiology and Molecular Biology, Sandia National Laboratories</h3>
+                <p>February 2021 - August 2022</p>
+                <ul>
+                    <li>Managed 25 high performing experimental staff, budgets of several million in projects and 10 separate sponsor relationships.</li>
+                    <li>Worked to maintain lab compliance, technological transfer and technological advancement patenting.</li>
+                    <li>Oversaw work in the fields of medical countermeasures, diagnostics, biofuels, advanced biological imaging and biomanufacturing.</li>
+                    <li>Managed interns, technologists, junior and highly senior staff. Facilitated their career growth. Delivered performance review and evaluation. Handled hiring and recruitment for the organization.</li>
+                </ul>
+            </div>
+            <div className="job">
+                <h3>Manager - Computational Biology and Biophysics, Sandia National Laboratories</h3>
+                <p>August 2019 - August 2022</p>
+                <ul>
+                    <li>Managed 20 high performing computational biology staff, budgets of several million in projects and 6 separate sponsor relationships.</li>
+                    <li>Initiated several major funding efforts, connecting capabilities in digital twinning of biological operations with customer needs.</li>
+                    <li>Set policy of software development and delivery, technological transfer and technological advancement patenting.</li>
+                    <li>Oversaw work in the fields of medical countermeasures, diagnostics, biofuels, advanced biological imaging and biomanufacturing.</li>
+                    <li>Managed interns, technologists, junior and highly senior staff. Facilitated their career growth. Delivered performance review and evaluation. Handled hiring and recruitment for the organization.</li>
+                    <li>Stood up Sandia\'s COVID diagnostics facility to provide diagnostic support for the US government.</li>
+                    <li>Worked with the US Office of Science and Technological Policy to connect cloud service providers and large high performance computing facilities with SARS-CoV2 researchers during the pandemic.</li> 
+                    <li>Provided insights to Operation Warp Speed on supply chain risk during the pandemic.</li>
+                </ul>
+            </div>
+            <div className="job">
+                <h3>Principal Member of the Technical Staff, Sandia National Laboratories</h3>
+                <p>June 2019 - August 2019</p>
+                <ul>
+                    <li>Took on a leadership position in DARPA\'s Entrepreneurial Investement Initiative to determine technological risk in biotechnology.</li>
+                    <li>Successfully secured several multimillion dollar funded efforts from the US Government to connect capabilites in digital twinning of biomanufacturing operations and computational genomics with real facility emulation.</li>
+                </ul>
+            </div>
+            <div className="job">
+                <h3>Senior Member of the Technical Staff, Sandia National Laboratories</h3>
+                <p>January 2015 - June 2019</p>
+                <ul>
+                    <li>Developed an open source software development platform for delivering computational biology tools to the broader research community.</li>
+                    <li>Made significant intellectual advancements in the areas of advanced algorithms, fully explainable machine learning, gene editing annotation, bacterial characterization and chemoinformatics.</li>
+                    <li>Board Member and Chair of the Center for Computational Biology and Genomic Medicine with the University of Illinois and the Mayo Clinic. Provided research insight and allocated research funds from member organizations.</li>\
+                    <li>Directly managed 2 Postdoctoral Appointees and 3 interns.</li>
+                    <li>Task Principal Investigator for DARPA\'s SAFE GENES - Safe Genomic Engineering Initiative</li>
+                    <li>Machine Learning Task Principal Investigator for Department of Energy\'s Co-optimization of Fuels and Engines.</li>
+                    <li>Received funding as the principal investigator on 5 large projects (each over $1M in funding) and oversaw the work and performance for over a dozen technical staff.</li>
+                </ul>
+            </div>
+            <div className="job">
+                <h3>Postdoctoral Appointee, Sandia National Laboratories</h3>
+                <p>May 2013 - January 2015</p>
+                <ul>
+                    <li>Developed genome annotation, chemoinformatics and bacterial characterization software.</li>
+                    <li>Published on bioengineering, biothreat detection and renewable chemical production.</li>
+                </ul>
+            </div>
+            <div className="job">
+                <h3>Graduate Research Assistant, Department of Animal Sciences, University of Missouri</h3>
+                <p>July 2012 - May 2013</p>
+                <ul>
+                    <li>Conducted research on the phylogenetics of metabolism, flux balance analysis and network biology.</li>
+                    <li>Published papers on the evolaution of metabolism in plants, mammals and cancers.</li>
+                </ul>
+            </div>
+            <div className="job">
+                <h3>National Library of Medicine Biomedical & Health Informatics Student Fellow, University of Missouri</h3>
+                <p>August 2009 - July 2012</p>
+                <ul>
+                    <li>Teaching assistant for courses in genomics, ecology, statistics and phylogenetics</li>
+                    <li>Published on comparative genomics, plant phylogeny and mammalian metabolism.</li>
                 </ul>
             </div>
         </section>
