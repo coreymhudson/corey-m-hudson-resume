f4fb1dbba0bb006d193cc2f8081e637af35973e6
Corey Hudson <coreymhudson@gmail.com>
2024-08-07 15:34:08 -0500

    Skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -5,12 +5,19 @@ const Skills = () => {
         <section className="skills">
             <h2>Key Skills</h2>
             <ul>
-                <li>Machine Learning</li>
+                <li>Project Management</li>
+                <li>Strategic Vision</li>
+                <li>Comparative and Functional Genomics</li>
+                <li>Network Biology</li>
+                <li>Biotechnology</li>
+                <li>Training and Mentoring</li>
+                <li>Software Delivery</li>
+                <li>Machine Learning and AI</li>
                 <li>Data Analysis</li>
                 <li>Statistical Modeling</li>
-                <li>Python, R</li>
+                <li>Programming: Python, R, SQL, C++, C, Perl</li>
+                <li>High Performance Computing: Linux/Unix, Cluster Computing, Cloud Infrastructure</li>
                 <li>Data Visualization</li>
-                <li>Project Management</li>
             </ul>
         </section>
     );
