1630b7fc9ab7c87216a2a5af8bad86ae0ea244c5
Corey Hudson <coreymhudson@gmail.com>
2024-08-07 15:42:37 -0500

    Adding pubs

---
+++ src/components/Research.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Research = () => {
+    return (
+        <section className="research">
+            <h2>Projects</h2>
+            <div className="research">
+                <h3>Project Title 1</h3>
+                <p>Brief description of the project.</p>
+            </div>
+            <div className="research">
+                <h3>Project Title 2</h3>
+                <p>Brief description of the project.</p>
+            </div>
+        </section>
+    );
+};
+
+export default Research;
