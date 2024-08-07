0d0813677f35d994f7510f8edd670fa5123af0d1
Corey Hudson <coreymhudson@gmail.com>
2024-08-07 15:44:35 -0500

    Adding pubs

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Experience from './components/Experience';
 import Education from './components/Education';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
+import Research from './components/Research';
 import Contact from './components/Contact';
 
 const App = () => {
@@ -21,6 +22,7 @@ const App = () => {
         <section id="education"><Education /></section>
         <section id="skills"><Skills /></section>
         <section id="projects"><Projects /></section>
+        <section id="research"><Research /></section>
         <section id="contact"><Contact /></section>
       </div>
     </div>
